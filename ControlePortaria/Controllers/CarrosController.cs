176aa718d0c1f82662fc7eb2f1003a9dd52278cc
italosanches <italo_carvalho157@hotmail.com>
2024-06-23 17:28:39 -0300

    criacao de controler e repository carro

---
+++ ControlePortaria/Controllers/CarrosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControlePortaria.Controllers
+{
+    public class CarrosController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ControlePortaria/Repository/CarroRepository.cs
@@ -0,0 +1,33 @@
+﻿using ControlePortaria.Context;
+using ControlePortaria.Models;
+using ControlePortaria.Repository.Interfaces;
+
+namespace ControlePortaria.Repository
+{
+    public class CarroRepository : ICarroRespotory
+
+    {
+        private readonly AppDbContext _context;
+        public CarroRepository(AppDbContext context)
+        {
+                _context = context;
+        }
+        public IEnumerable<Carro> Carros => _context.Carros.ToArray();
+
+        public void Create(Carro carro)
+        {
+            _context.Carros.Add(carro);
+            _context.SaveChanges();
+        }
+
+        public Carro GetCarroById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Carro carro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
