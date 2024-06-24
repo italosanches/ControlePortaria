b075eaf447621a8f14bad6437e2397343f30c9a3
italosanches <italo_carvalho157@hotmail.com>
2024-06-24 10:17:51 -0300

    Ajuste no nome da classe

---
+++ ControlePortaria/Repository/Interfaces/ICarroRepository.cs
@@ -0,0 +1,13 @@
+﻿using ControlePortaria.Models;
+
+namespace ControlePortaria.Repository.Interfaces
+{
+    public interface ICarroRepository
+    {
+        IEnumerable<Carro> Carros { get; }
+        public Carro GetCarroById(int id);
+
+        public void Create(Carro carro);
+        public void Update(Carro carro);
+    }
+}
